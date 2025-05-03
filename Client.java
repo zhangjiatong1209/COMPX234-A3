b28263e6d2fca148b113089c314d0ab0e0ca1249
zhangjiatong1209 <535506359@qq.com>
2025-05-03 21:04:19 +0800

    forth（change client，Add file path）

--- Client.java
+++ Client.java
@@ -5,9 +5,13 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-
+/**
+ * 客户端类，用于向服务器发送文件路径请求并接收响应
+ */
 public class Client {
+    // 服务器主机地址
     private static final String SERVER_HOST = "localhost";
+    // 服务器端口号
     private static final int SERVER_PORT = 51234;
 
     /**
@@ -21,23 +25,36 @@ public class Client {
              PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
              BufferedReader fileReader = new BufferedReader(new FileReader(filePath))) {
             String line;
+            // 逐行读取文件内容并发送给服务器
             while ((line = fileReader.readLine()) != null) {
                 out.println(line);
+                // 接收服务器的响应
                 String response = in.readLine();
+                // 打印本地发送的内容和服务器的响应
                 System.out.println(line + ": " + response);
             }
         } catch (IOException e) {
+            // 打印异常堆栈信息，实际应用中可以更详细地记录日志等
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.err.println("Usage: java Client <request_file_path1> [<request_file_path2>...]");
-            System.exit(1);
-        }
+        // 直接在代码中指定十个测试文件路径
+        String[] filePaths = {
+            "client_1.txt",
+            "client_2.txt",
+            "client_3.txt",
+            "client_4.txt",
+            "client_5.txt",
+            "client_6.txt",
+            "client_7.txt",
+            "client_8.txt",
+            "client_9.txt",
+            "client_10.txt"
+        };
         Client client = new Client();
-        for (String filePath : args) {
+        for (String filePath : filePaths) {
             client.sendRequests(filePath);
         }
     }
