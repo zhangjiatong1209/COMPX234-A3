1ec43c83dfff140018c94de80ed62fd316d3a7bf
zhangjiatong1209 <535506359@qq.com>
2025-05-07 16:59:41 +0800

    twelfth（explain tuple)

--- Tuple.java
+++ Tuple.java
@@ -1,22 +1,27 @@
 import java.util.Objects;
 
 public class Tuple {
+      // The key of the tuple. It is declared as final, meaning it cannot be changed after initialization.
     private final String key;
     private String value;
 
+// Constructor to initialize a new tuple with a given key and value.
     public Tuple(String key, String value) {
         this.key = key;
         this.value = value;
     }
 
+    // Getter method to retrieve the key of the tuple.
     public String getKey() {
         return key;
     }
 
+     // Getter method to retrieve the value of the tuple.
     public String getValue() {
         return value;
     }
 
+    // Setter method to update the value of the tuple
     public void setValue(String value) {
         this.value = value;
     }
